02b3aa9b6a2a10f431094bfc8cd4bf7fad67cdb0
Krutagna Patel <kpatel.kp7016@gmail.com>
2023-03-11 18:25:11 +0530

    card styling completed

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,15 +1,23 @@
 import "./Card.css";
 import iconDice from "../../assets/icon-dice.svg";
+import dividerMobile from "../../assets/pattern-divider-mobile.svg";
+import dividerDesktop from "../../assets/pattern-divider-desktop.svg";
 
 const Card = ({ data, onClick }) => {
   return (
-    <div className="card">
-      <p className="card__advice-number">Advice Number: {data.id}</p>
-      <h1 className="card__advice">{data.advice}</h1>
-      <button className="card__btn" onClick={onClick}>
-        <img src={iconDice} alt="Dice" />
-      </button>
-    </div>
+    <section className="card-section">
+      <div className="card grid grid--items-center background-grayish-blue">
+        <p className="card__advice-number">Advice #{data.id}</p>
+        <h1 className="card__advice">"{data.advice}"</h1>
+        <picture>
+          <source media="(min-width: 48em)" src={dividerDesktop} />
+          <img src={dividerMobile} aria-hidden="true" alt="" />
+        </picture>
+        <button className="card__btn" onClick={onClick}>
+          <img src={iconDice} alt="Dice" />
+        </button>
+      </div>
+    </section>
   );
 };
 
