9f1d4b97f932dd3c433d6454921168cf5f809169
Krutagna Patel <kpatel.kp7016@gmail.com>
2023-03-11 16:21:08 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+const url = "https://api.adviceslip.com/advice";
 
 function App() {
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url, { cache: "no-cache" });
+      const data = await response.json();
+      setData(data.slip);
+      console.log(data.slip);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleClick = () => {
+    fetchData();
+  };
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app" style={{ maxWidth: "32rem" }}>
+      <p>Advice Number: {data.id}</p>
+      <h1>{data.advice}</h1>
+      <button onClick={handleClick}>Generate Advice</button>
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import "./Card.css";
+
+const Card = () => {
+  return (
+    <div>
+      <p>Card</p>
+    </div>
+  );
+};
+
+export default Card;
